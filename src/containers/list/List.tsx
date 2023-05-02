7bb93ea336b0b89a7c84e19ad55d98710ffe064e
john-reiner <john.reiner@datadoghq.com>
2023-05-02 09:26:43 -0400

    set up a list container for the App.tsx file to read from

---
+++ src/containers/list/List.tsx
@@ -0,0 +1,18 @@
+import React, {useState, useEffect} from 'react'
+
+
+
+interface ListProps {
+}
+
+export default function List({
+    
+}: ListProps) {
+
+    useEffect(() => {
+    }, []);
+
+    return (
+        <p>List</p>
+    )
+}
