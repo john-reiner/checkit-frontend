fd816a150294cb88dc5c04871af25fdf0c5f3b45
john-reiner <john.reiner@datadoghq.com>
2023-05-03 08:25:31 -0400

    update a task name

--- src/packages/Task/Task.tsx
+++ src/packages/Task/Task.tsx
@@ -1,7 +1,8 @@
 import React, {useState, useEffect} from 'react'
 import { TaskType } from './types/TaskType'
+import { IconAdjustments } from '@tabler/icons-react';
 
-import { Checkbox } from '@mantine/core';
+import { ActionIcon, Center, Checkbox, Group, Space, TextInput } from '@mantine/core';
 
 interface TaskProps {
     taskProps: TaskType 
@@ -16,6 +17,8 @@ export default function Task({
         completed: taskProps.completed
     })
 
+    const [editName, setEditName] = useState<boolean>(false)
+
     const handleCheck = (
 
     ) => {
@@ -23,7 +26,39 @@ export default function Task({
         setTask({...task, completed: newCompletedState})
     }
 
+    const handleTaskChange = (
+        e: React.ChangeEvent<HTMLInputElement>
+    ) => {
+        setTask({...task, [e.target.name]: e.target.value})
+    }
+
+    const handleSubmit = (
+        e: React.FormEvent<HTMLFormElement>
+    ) => {
+        e.preventDefault()
+        setEditName(false)
+    }
+
     return (
-        <Checkbox checked={task.completed} label={task.name} onChange={handleCheck} />
+
+        <Group>
+            {editName ? 
+                <form onSubmit={handleSubmit}>
+                    <TextInput
+                        placeholder="New Task"
+                        variant="filled"
+                        radius="xl"
+                        value={task.name}
+                        onChange={handleTaskChange}
+                        name='name'
+                    />                    
+                </form> :
+            <Checkbox checked={task.completed} label={task.name} onChange={handleCheck} />
+        }
+            <Space w="md" />
+            <ActionIcon color="blue" radius="xl" variant="outline" onClick={() => setEditName(!editName)}>
+            <   IconAdjustments size="1.125rem" />
+            </ActionIcon>
+        </Group>
     )
 }
